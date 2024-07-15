16c4b6822339c5db0b9b9a66bf7487b711547f92
CodePanda-crypto <kadufluffy@gmail.com>
2024-07-15 12:50:44 +0500

    Update reduced redundancy and use of State

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,13 @@
-/* eslint-disable no-unused-vars */
-
-import React from 'react';
+import React, { useState } from 'react';
 import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = React.useState(0);
+  const [count, setCount] = useState(0);
 
   return (
     <div className="container">
       <h1>Hello Vite + React!</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-
-      <img src={viteLogo} className="logo" alt="Vite logo" />
-
-      <h1>Vite + React</h1>
-
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -31,11 +16,12 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <img src={viteLogo} className="logo" alt="Vite logo" />
     </div>
   );
 }
+
 export default App;
