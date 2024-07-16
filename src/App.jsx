085e2c66eb0a9160cd7e9e68976a524c3ab5b47c
CodePanda-crypto <kadufluffy@gmail.com>
2024-07-16 10:54:00 +0500

    main component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import Header from './Components/header';
 import Footer from './Components/footer';
+import Main from './Components/main';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,18 +10,7 @@ function App() {
   return (
     <div className="container">
       <Header />
-      <main className="card">
-        <h1>Fun facts about React</h1>
-        <div className="react-facts">
-          <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100K stars on GitHub</li>
-            <li>Is maintained by Facebook</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-          </ul>
-        </div>
-      </main>
+      <Main />
       <Footer />
     </div>
   );
---
+++ src/Components/main.jsx
@@ -0,0 +1,19 @@
+import './main.css';
+
+function Main() {
+  return (
+    <main className="card">
+      <h1>Fun facts about React</h1>
+      <div className="react-facts">
+        <ul>
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 100K stars on GitHub</li>
+          <li>Is maintained by Facebook</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+      </div>
+    </main>
+  );
+}
+export default Main;
