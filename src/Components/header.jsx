8dc9ed288c6a0256ce148f463e37dc5c39118597
CodePanda-crypto <kadufluffy@gmail.com>
2024-07-16 14:01:45 +0500

    used grid to design the layout; link styling and transitions

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -2,21 +2,35 @@ import './header.css';
 function Header() {
   return (
     <header className="App-header">
-      <nav>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src="/react.svg"
-            className="App-logo react logo-spin"
-            alt="React logo"
-          />
-        </a>
+      <nav className="navbar">
+        <div>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="/react.svg"
+              className="App-logo react logo-spin"
+              alt="React logo"
+            />
+          </a>
+        </div>
+        <div>
+          <ul className="nav-links">
+            <li className="nav-link">
+              <a href="#">Pricing</a>
+            </li>
+            <li className="nav-link">
+              <a href="#">About</a>
+            </li>
+            <li className="nav-link">
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
       </nav>
-      <h1>ReactFacts</h1>
     </header>
   );
 }
