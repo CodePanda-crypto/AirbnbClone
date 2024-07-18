2fe8fe586be0d9a91a8505dbf8c813617d513914
CodePanda-crypto <kadufluffy@gmail.com>
2024-07-18 11:14:38 +0500

    updated media queries to add responsiveness to the page

--- src/Components/main.jsx
+++ src/Components/main.jsx
@@ -10,7 +10,7 @@ export default function Main() {
         title="ONLINE"
         img="/Mountain-Bike.png"
         rating="4.8"
-        views="(2)."
+        views="(25)."
         country="USA"
         description="Join Group Mountain Biking"
         price="From $100"
@@ -35,30 +35,30 @@ export default function Main() {
       />
       <Card
         title="JOIN NOW"
-        img="/mountain-bike.jpg"
-        rating="4.8"
-        views="(2)."
+        img="/travel.jpg"
+        rating="4.9"
+        views="(35)."
         country="USA"
-        description="Join Group Mountain Biking"
-        price="From $50"
+        description="Explore the World with us"
+        price="From $500"
       />
       <Card
         title="ONLINE"
-        img="/wedding-photography.png"
+        img="/group-hiking.jpg"
         rating="5.0"
         views="(30)."
         country="USA"
-        description="Learn wedding photography"
+        description="Join Group Hiking"
         price="From $125"
       />
       <Card
-        title="SOLD OUT"
-        img="/image.png"
+        title="Favorite"
+        img="/Nature.jpg"
         rating="5.0"
-        views="(6)."
-        country="USA"
-        description="Life lessons with Katie Zaferes"
-        price="From $136"
+        views="(45)."
+        country="Austria"
+        description="Take a spin around the world"
+        price="From $400"
       />
     </main>
   );
