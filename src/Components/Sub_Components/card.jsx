8f377c2aeeaa174b7936e0e3fab856987cffcf43
CodePanda-crypto <kadufluffy@gmail.com>
2024-07-18 12:14:22 +0500

    prop destructuring

--- src/Components/Sub_Components/card.jsx
+++ src/Components/Sub_Components/card.jsx
@@ -1,24 +1,32 @@
 import './card.css';
 
-export default function Card(props) {
+export default function Card({
+  title,
+  img,
+  rating,
+  views,
+  country,
+  description,
+  price,
+}) {
   return (
     <section className="card-container">
       <div className="card-header">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
       </div>
       <div className="card-body">
-        <img src={props.img} alt={props.description} className="card-image" />
+        <img src={img} alt={description} className="card-image" />
       </div>
       <div className="card-footer">
         <div className="card-stats">
           <img src="/Star.svg" alt="Star" />
-          <small className="rating">{props.rating}</small>
-          <small>{props.views}</small>
-          <small>{props.country}</small>
+          <small className="rating">{rating}</small>
+          <small>{views}</small>
+          <small>{country}</small>
         </div>
-        <h2>{props.description}</h2>
+        <h2>{description}</h2>
         <p>
-          <span className="price">{props.price} </span>/ person
+          <span className="price">{price} </span>/ person
         </p>
       </div>
     </section>
